17b3eec8557f002e467f44bcef13e3c3803853a4
skylinersandre <43538922+skylinersandre@users.noreply.github.com>
2020-06-02 16:23:56 +0200

    Enum Zones

---
+++ app/src/main/java/com/cool/pulseit/utils/Zones.java
@@ -0,0 +1,47 @@
+package com.cool.pulseit.utils;
+
+public enum Zones {
+    VERYLIGHT ("Gesundheitszone"),
+    LIGHT ("Fettverbrennungszone"),
+    MODERATE("aerobe Zone"),
+    HARD ("anaerobe Zone"),
+    VERYHARD ("rote Zone");
+
+    public static Zones toEnum(String zonesString) {
+        switch (zonesString) {
+            case "Gesundheitszone":
+            case "VERYLIGHT":
+                return Zones.VERYLIGHT;
+            case "Fettverbrennungszone":
+            case "LIGHT":
+                return Zones.LIGHT;
+            case "aerobe Zone":
+            case "MODERATE":
+                return Zones.MODERATE;
+            case "anaerobe Zone":
+            case "HARD":
+                return Zones.HARD;
+            case "rote Zone":
+            case "VERYHARD":
+                return Zones.VERYHARD;
+            default:
+                throw new IllegalArgumentException(String.format("Can't convert %s to ZonesEnum", zonesString));
+        }
+    }
+
+    private String value;
+
+    Zones(final String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return this.getValue();
+    }
+
+}
