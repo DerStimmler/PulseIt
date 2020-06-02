b60db11425a4926fa146908cab541d0e32c764f7
skylinersandre <43538922+skylinersandre@users.noreply.github.com>
2020-06-02 14:05:16 +0200

    added dialog to history_fragment for detail view

--- app/src/main/java/com/cool/pulseit/PulsesAdapter.java
+++ app/src/main/java/com/cool/pulseit/PulsesAdapter.java
@@ -1,11 +1,14 @@
 package com.cool.pulseit;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,6 +19,8 @@ import com.cool.pulseit.utils.DateFormatter;
 import com.cool.pulseit.utils.Result;
 import com.cool.pulseit.utils.StatusSnackbar;
 
+import org.w3c.dom.Text;
+
 import java.util.List;
 
 public class PulsesAdapter extends RecyclerView.Adapter<PulsesViewHolder> {
@@ -23,8 +28,10 @@ public class PulsesAdapter extends RecyclerView.Adapter<PulsesViewHolder> {
     private List<Pulse> _pulses;
     private Context _context;
     private View _mainActivity;
+    private Dialog myDialog;
 
-    public PulsesAdapter(List<Pulse> pulses) {
+    public PulsesAdapter(List<Pulse> pulses){
+
         _pulses = pulses;
     }
 
@@ -37,7 +44,25 @@ public class PulsesAdapter extends RecyclerView.Adapter<PulsesViewHolder> {
 
         View historyView = inflater.inflate(R.layout.history_row, parent, false);
 
-        PulsesViewHolder viewHolder = new PulsesViewHolder(historyView);
+        final PulsesViewHolder viewHolder = new PulsesViewHolder(historyView);
+
+        // Dialog ini
+        myDialog = new Dialog(_context);
+        myDialog.setContentView(R.layout.dialog_history);
+
+        viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView dialog_pulse_tv = (TextView) myDialog.findViewById(R.id.dialog_pulse_id);
+                TextView dialog_date_tv = (TextView) myDialog.findViewById(R.id.dialog_date_id);
+
+                Pulse pulse = _pulses.get(viewHolder.getAdapterPosition());
+                dialog_pulse_tv.setText(String.valueOf(pulse.pulse));
+                dialog_date_tv.setText(DateFormatter.forUi(pulse.date));
+                Toast.makeText(_context, "Test Click" + String.valueOf(viewHolder.getAdapterPosition()), Toast.LENGTH_SHORT).show(); //Todo: raus löschen am ende
+                myDialog.show();
+            }
+        });
 
         return viewHolder;
     }
