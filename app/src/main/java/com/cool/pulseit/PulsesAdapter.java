237cc838c2e13fb22fa40eb9c6de476d27e1c3e2
Tim <43130816+DerStimmler@users.noreply.github.com>
2020-06-04 00:50:24 +0200

    refactored detail dialog

--- app/src/main/java/com/cool/pulseit/PulsesAdapter.java
+++ app/src/main/java/com/cool/pulseit/PulsesAdapter.java
@@ -1,25 +1,23 @@
 package com.cool.pulseit;
 
 import android.app.Activity;
-import android.app.Dialog;
 import android.content.Context;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.cool.pulseit.database.DatabaseManager;
 import com.cool.pulseit.entities.Pulse;
+import com.cool.pulseit.fragments.DetailDialogFragment;
 import com.cool.pulseit.utils.DateFormatter;
 import com.cool.pulseit.utils.Result;
 import com.cool.pulseit.utils.StatusSnackbar;
-import com.github.mikephil.charting.charts.BarChart;
 
 import java.util.List;
 
@@ -28,7 +26,6 @@ public class PulsesAdapter extends RecyclerView.Adapter<PulsesViewHolder> {
     private List<Pulse> _pulses;
     private Context _context;
     private View _mainActivity;
-    private Dialog myDialog;
 
     public PulsesAdapter(List<Pulse> pulses){
 
@@ -40,54 +37,19 @@ public class PulsesAdapter extends RecyclerView.Adapter<PulsesViewHolder> {
     public PulsesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         _context = parent.getContext();
         _mainActivity = parent.getRootView();
-        LayoutInflater inflater = LayoutInflater.from(_context);
+        final LayoutInflater inflater = LayoutInflater.from(_context);
 
-        View historyView = inflater.inflate(R.layout.history_row, parent, false);
+        View historyView = inflater.inflate(R.layout.history_item, parent, false);
 
         final PulsesViewHolder viewHolder = new PulsesViewHolder(historyView);
-
-        // Dialog ini
-        myDialog = new Dialog(_context);
-        myDialog.setContentView(R.layout.dialog_history);
-        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 
         viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Darstellung erster Card ->Puls+Datum+Zone
-                TextView dialog_pulse_tv = myDialog.findViewById(R.id.dialog_pulse_id);
-                TextView dialog_date_tv = myDialog.findViewById(R.id.dialog_date_id);
-                TextView dialog_zones_tv = myDialog.findViewById(R.id.dialog_zones_id);
-
                 Pulse pulse = _pulses.get(viewHolder.getAdapterPosition());
-                dialog_pulse_tv.setText(String.valueOf(pulse.pulse));
-                dialog_date_tv.setText(DateFormatter.forUi(pulse.date));
-
-                ZoneCalculator zoneCalculator = new ZoneCalculator(pulse.pulse,pulse.settings.age,pulse.settings.weight, pulse.settings.gender);
-                String zone = zoneCalculator.calculateZone();
-                dialog_zones_tv.setText(zone);
-
-                //Dastellung zweiter Card -> Settings
-                TextView dialog_gender_tv = myDialog.findViewById(R.id.dialog_settings_gender);
-                TextView dialog_age_tv = myDialog.findViewById(R.id.dialog_settings_age);
-                TextView dialog_weight_tv = myDialog.findViewById(R.id.dialog_settings_weight);
-
-                dialog_gender_tv.setText(String.valueOf(pulse.settings.gender));
-                dialog_age_tv.setText(String.valueOf(pulse.settings.age));
-                dialog_weight_tv.setText(String.valueOf(pulse.settings.weight));
-
-
-                BarChart chart = myDialog.findViewById(R.id.detail_chart);
-                MaximumHeartRateCalculator mhrCalculator = new MaximumHeartRateCalculator(pulse.settings.age, pulse.settings.weight, pulse.settings.gender);
-                int mhr = mhrCalculator.calculateMaximumHeartRate();
-                ChartGenerator cg = new ChartGenerator(_context);
-
-                chart = cg.classifyPulseChart(chart, pulse, mhr);
-
-                chart.invalidate();
-
-                Toast.makeText(_context, "Test Click" + String.valueOf(viewHolder.getAdapterPosition()), Toast.LENGTH_SHORT).show(); //Todo: raus löschen am ende
-                myDialog.show();
+                FragmentManager fm = ((FragmentActivity)_context).getSupportFragmentManager();
+                DetailDialogFragment dialog = new DetailDialogFragment(pulse);
+                dialog.show(fm, "");
             }
         });
 
---
+++ app/src/main/java/com/cool/pulseit/fragments/DetailDialogFragment.java
@@ -0,0 +1,76 @@
+package com.cool.pulseit.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.cool.pulseit.ChartGenerator;
+import com.cool.pulseit.MaximumHeartRateCalculator;
+import com.cool.pulseit.R;
+import com.cool.pulseit.ZoneCalculator;
+import com.cool.pulseit.entities.Pulse;
+import com.cool.pulseit.utils.DateFormatter;
+import com.github.mikephil.charting.charts.BarChart;
+
+public class DetailDialogFragment extends DialogFragment {
+
+    private final Pulse _pulse;
+    private View _view;
+
+    public DetailDialogFragment(Pulse pulse) {
+        _pulse = pulse;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        _view = inflater.inflate(R.layout.detail_dialog, container, false);
+        getDialog().setTitle("Details");
+
+        init();
+
+        return _view;
+    }
+
+    private void init() {
+        // Darstellung erster Card ->Puls+Datum+Zone
+        TextView dialog_pulse_tv = _view.findViewById(R.id.dialog_pulse_id);
+        TextView dialog_date_tv = _view.findViewById(R.id.dialog_date_id);
+        TextView dialog_zones_tv = _view.findViewById(R.id.dialog_zones_id);
+
+
+        dialog_pulse_tv.setText(String.valueOf(_pulse.pulse));
+        dialog_date_tv.setText(DateFormatter.forUi(_pulse.date));
+
+        ZoneCalculator zoneCalculator = new ZoneCalculator(_pulse.pulse,_pulse.settings.age,_pulse.settings.weight, _pulse.settings.gender);
+        String zone = zoneCalculator.calculateZone();
+        dialog_zones_tv.setText(zone);
+
+        //Dastellung zweiter Card -> Settings
+        TextView dialog_gender_tv = _view.findViewById(R.id.dialog_settings_gender);
+        TextView dialog_age_tv = _view.findViewById(R.id.dialog_settings_age);
+        TextView dialog_weight_tv = _view.findViewById(R.id.dialog_settings_weight);
+
+        dialog_gender_tv.setText(String.valueOf(_pulse.settings.gender));
+        dialog_age_tv.setText(String.valueOf(_pulse.settings.age));
+        dialog_weight_tv.setText(String.valueOf(_pulse.settings.weight));
+
+
+        BarChart chart = _view.findViewById(R.id.detail_chart);
+        MaximumHeartRateCalculator mhrCalculator = new MaximumHeartRateCalculator(_pulse.settings.age, _pulse.settings.weight, _pulse.settings.gender);
+        int mhr = mhrCalculator.calculateMaximumHeartRate();
+        ChartGenerator cg = new ChartGenerator(getContext());
+
+        chart = cg.classifyPulseChart(chart, _pulse, mhr);
+
+        chart.invalidate();
+    }
+
+
+}
