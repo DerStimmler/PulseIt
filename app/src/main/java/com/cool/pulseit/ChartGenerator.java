1f9167beb8a52fb5eead94ee9b4e1c5778ae4b05
Tim <43130816+DerStimmler@users.noreply.github.com>
2020-06-04 13:30:04 +0200

    refactored and improved chart generator

--- app/src/main/java/com/cool/pulseit/ChartGenerator.java
+++ app/src/main/java/com/cool/pulseit/ChartGenerator.java
@@ -24,56 +24,112 @@ public class ChartGenerator {
     private Context _context;
     private String[] _stackLabels;
 
-    public ChartGenerator(Context context){
+    public ChartGenerator(Context context) {
         _context = context;
         _colors = getColours();
         _stackLabels = getStackLabels();
     }
 
-    private String[] getStackLabels() {
-        return new String[]{Zones.VERYLIGHT.getValue(),Zones.LIGHT.getValue(),Zones.MODERATE.getValue(),Zones.HARD.getValue(), Zones.VERYHARD.getValue()};
-    }
-
-    private int[] getColours() {
-        return new int[]{Color.TRANSPARENT,ResourcesCompat.getColor(_context.getResources(), R.color.grey, null), ResourcesCompat.getColor(_context.getResources(), R.color.turquoise, null),ResourcesCompat.getColor(_context.getResources(), R.color.green, null),ResourcesCompat.getColor(_context.getResources(), R.color.orange, null),ResourcesCompat.getColor(_context.getResources(), R.color.red, null)};
-    }
-
-    public BarChart classifyPulseChart(BarChart chart, Pulse pulse, int maxPulse){
+    public BarChart classifyPulseChart(BarChart chart, Pulse pulse, int maxPulse) {
 
         List<BarEntry> entries = getEntries(maxPulse);
 
-        BarDataSet dataSet = new BarDataSet(entries,"Label");
-        dataSet.setColors(_colors);
-        dataSet.setStackLabels(_stackLabels);
-        dataSet.setValueTextColor(Color.TRANSPARENT);
+        BarDataSet dataSet = createBarDataSet(entries);
 
+        BarData bardata = createBarData(dataSet);
+
+        addLimitLines(chart, entries, pulse);
+
+        chart.setData(bardata);
+        setStandardChartOptions(chart);
+
+        return chart;
+    }
+
+    public BarChart generateInfoChart(BarChart chart) {
+
+        List<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(1, new float[]{50f, 10f, 10f, 10f, 10f, 10f}));
+
+        BarDataSet dataSet = createBarDataSet(entries);
+
+        BarData bardata = createBarData(dataSet);
+
+        addLimitLines(chart, entries, null);
+
+        chart.setData(bardata);
+        setStandardChartOptions(chart);
+        chart.getAxisLeft().setValueFormatter(new PercentFormatter());
+
+        return chart;
+    }
+
+    private String[] getStackLabels() {
+        return new String[]{Zones.VERYLIGHT.getValue(), Zones.LIGHT.getValue(), Zones.MODERATE.getValue(), Zones.HARD.getValue(), Zones.VERYHARD.getValue()};
+    }
+
+    private int[] getColours() {
+        return new int[]{Color.TRANSPARENT, ResourcesCompat.getColor(_context.getResources(), R.color.grey, null), ResourcesCompat.getColor(_context.getResources(), R.color.turquoise, null), ResourcesCompat.getColor(_context.getResources(), R.color.green, null), ResourcesCompat.getColor(_context.getResources(), R.color.orange, null), ResourcesCompat.getColor(_context.getResources(), R.color.red, null)};
+    }
+
+    private void addLimitLines(BarChart chart, List<BarEntry> entries, Pulse pulse) {
+        LimitLine ll;
+        float x = entries.get(0).getYVals()[0];
+        ll = new LimitLine(x);
+        ll.setLineColor(Color.LTGRAY);
+        chart.getAxisLeft().addLimitLine(ll);
+        ll = new LimitLine(entries.get(0).getYVals()[1] + x);
+        ll.setLineColor(Color.LTGRAY);
+        chart.getAxisLeft().addLimitLine(ll);
+        ll = new LimitLine(entries.get(0).getYVals()[1] / 2 + x, Zones.VERYLIGHT.getValue());
+        ll.setLineColor(Color.TRANSPARENT);
+        chart.getAxisLeft().addLimitLine(ll);
+        x += entries.get(0).getYVals()[1];
+        ll = new LimitLine(entries.get(0).getYVals()[2] + x);
+        ll.setLineColor(Color.LTGRAY);
+        chart.getAxisLeft().addLimitLine(ll);
+        ll = new LimitLine(entries.get(0).getYVals()[2] / 2 + x, Zones.LIGHT.getValue());
+        ll.setLineColor(Color.TRANSPARENT);
+        chart.getAxisLeft().addLimitLine(ll);
+        x += entries.get(0).getYVals()[2];
+        ll = new LimitLine(entries.get(0).getYVals()[3] + x);
+        ll.setLineColor(Color.LTGRAY);
+        chart.getAxisLeft().addLimitLine(ll);
+        ll = new LimitLine(entries.get(0).getYVals()[3] / 2 + x, Zones.MODERATE.getValue());
+        ll.setLineColor(Color.TRANSPARENT);
+        chart.getAxisLeft().addLimitLine(ll);
+        x += entries.get(0).getYVals()[3];
+        ll = new LimitLine(entries.get(0).getYVals()[4] + x);
+        ll.setLineColor(Color.LTGRAY);
+        chart.getAxisLeft().addLimitLine(ll);
+        ll = new LimitLine(entries.get(0).getYVals()[4] / 2 + x, Zones.HARD.getValue());
+        ll.setLineColor(Color.TRANSPARENT);
+        chart.getAxisLeft().addLimitLine(ll);
+        x += entries.get(0).getYVals()[4];
+        ll = new LimitLine(entries.get(0).getYVals()[5] + x);
+        ll.setLineColor(Color.LTGRAY);
+        chart.getAxisLeft().addLimitLine(ll);
+        ll = new LimitLine(entries.get(0).getYVals()[5] / 2 + x, Zones.VERYHARD.getValue());
+        ll.setLineColor(Color.TRANSPARENT);
+        chart.getAxisLeft().addLimitLine(ll);
+        x += entries.get(0).getYVals()[5];
+        if (pulse != null) {
+            ll = new LimitLine(pulse.pulse, "Eigener Puls");
+            chart.getAxisLeft().addLimitLine(ll);
+        }
+    }
+
+    private BarData createBarData(BarDataSet dataSet) {
         BarData bardata = new BarData(dataSet);
         bardata.setBarWidth(0.5f);
 
-        LimitLine ll;
-        float x = entries.get(0).getYVals()[0];
-        ll = new LimitLine(entries.get(0).getYVals()[1] + x, Zones.VERYLIGHT.getValue());
-        chart.getAxisLeft().addLimitLine(ll);
-        x += entries.get(0).getYVals()[1];
-        ll = new LimitLine(entries.get(0).getYVals()[2] + x, Zones.LIGHT.getValue());
-        chart.getAxisLeft().addLimitLine(ll);
-        x += entries.get(0).getYVals()[2];
-        ll = new LimitLine(entries.get(0).getYVals()[3] + x, Zones.MODERATE.getValue());
-        chart.getAxisLeft().addLimitLine(ll);
-        x += entries.get(0).getYVals()[3];
-        ll = new LimitLine(entries.get(0).getYVals()[4] + x, Zones.HARD.getValue());
-        chart.getAxisLeft().addLimitLine(ll);
-        x += entries.get(0).getYVals()[4];
-        ll = new LimitLine(entries.get(0).getYVals()[5] + x, Zones.VERYHARD.getValue());
-        chart.getAxisLeft().addLimitLine(ll);
-        x += entries.get(0).getYVals()[5];
-        ll = new LimitLine(pulse.pulse, "Eigener Puls");
-        chart.getAxisLeft().addLimitLine(ll);
+        return bardata;
+    }
 
-        chart.setData(bardata);
-        chart.getXAxis().setAxisMaximum(1f);
+    private void setStandardChartOptions(BarChart chart) {
+        chart.getXAxis().setAxisMaximum(0.2f);
         chart.getXAxis().setAxisMinimum(1f);
-        chart.getXAxis().setLabelCount(0,true);
+        chart.getXAxis().setLabelCount(0, true);
         chart.getAxisLeft().setAxisMinimum(0f);
         chart.getAxisRight().setEnabled(false);
         chart.getXAxis().setGranularityEnabled(true);
@@ -82,9 +138,14 @@ public class ChartGenerator {
         chart.getXAxis().setPosition(XAxis.XAxisPosition.TOP_INSIDE);
         chart.setHighlightFullBarEnabled(false);
         chart.getData().setHighlightEnabled(false);
-        chart.setFitBars(true);
+        chart.setDrawBarShadow(false);
+        chart.getLegend().setEnabled(false);
+        chart.getDescription().setEnabled(false);
+        chart.getXAxis().setSpaceMin(0);
+        chart.getXAxis().setSpaceMax(0);
+        chart.setBackgroundColor(Color.WHITE);
 
-        return chart;
+        chart.setFitBars(true);
     }
 
     private List<BarEntry> getEntries(int maxPulse) {
@@ -104,46 +165,14 @@ public class ChartGenerator {
         return entries;
     }
 
-    public BarChart generateInfoChart(BarChart chart){
-
-        List<BarEntry> entries = new ArrayList<>();
-        entries.add(new BarEntry(1,new float[]{50f,10f,10f,10f,10f,10f}));
-
-        BarDataSet dataSet = new BarDataSet(entries,"Label");
+    private BarDataSet createBarDataSet(List<BarEntry> entries) {
+        BarDataSet dataSet = new BarDataSet(entries, "Label");
         dataSet.setColors(_colors);
         dataSet.setStackLabels(_stackLabels);
         dataSet.setValueTextColor(Color.TRANSPARENT);
 
-        BarData bardata = new BarData(dataSet);
-        bardata.setBarWidth(0.5f);
-
-        LimitLine ll;
-        ll = new LimitLine(60f, Zones.VERYLIGHT.getValue());
-        chart.getAxisLeft().addLimitLine(ll);
-        ll = new LimitLine(70f, Zones.LIGHT.getValue());
-        chart.getAxisLeft().addLimitLine(ll);
-        ll = new LimitLine(80f, Zones.MODERATE.getValue());
-        chart.getAxisLeft().addLimitLine(ll);
-        ll = new LimitLine(90f, Zones.HARD.getValue());
-        chart.getAxisLeft().addLimitLine(ll);
-        ll = new LimitLine(100f, Zones.VERYHARD.getValue());
-        chart.getAxisLeft().addLimitLine(ll);
-
-        chart.setData(bardata);
-        chart.getXAxis().setAxisMaximum(1f);
-        chart.getXAxis().setAxisMinimum(1f);
-        chart.getXAxis().setLabelCount(0,true);
-        chart.getAxisLeft().setAxisMinimum(0f);
-        chart.getAxisLeft().setAxisMaximum(110f);
-        chart.getAxisRight().setEnabled(false);
-        chart.getXAxis().setGranularityEnabled(true);
-        chart.getXAxis().setGranularity(1f);
-        chart.getXAxis().setDrawLabels(false);
-        chart.getAxisLeft().setValueFormatter(new PercentFormatter());
-        chart.setHighlightFullBarEnabled(false);
-        chart.getData().setHighlightEnabled(false);
-        chart.setFitBars(true);
-
-        return chart;
+        return dataSet;
     }
+
+
 }
